59abbf71a54ca48831ba1889c98a2545987fb1cb
erdemberaa <erdembera18@gmail.com>
2025-05-14 18:35:20 +0300

    first commit

---
+++ courseapp/courses/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('anasayfa')
+def urunler (request):
+    return HttpResponse('Ürünler')
