cbbe76b29a7b13c2797b19efb4f50ae7f593ae67
erdemberaa <erdembera18@gmail.com>
2025-05-17 02:32:16 +0300

    ufak tefek url update

--- courseapp/courses/urls.py
+++ courseapp/courses/urls.py
@@ -3,12 +3,16 @@ from . import views
 
 # http://127.0.0.1:8000/client          => anasayfa
 # http://127.0.0.1:8008/client/home     => anasayfa
-# http://127.0.0.1:8000/client/products  => ürünler
+# http://127.0.0.1:8000/client/products  => kurslar
 
 
 
 urlpatterns = [
     path('', views.home),
     path('anasayfa', views.home),
-    path('ürünler', views.ürünler),
+    path('kurslar', views.kurslar),
+    path('urunler', views.urunler),
+    path('<kurs_adi>', views.hakkında),
+    path('kategori/<int:category_name>', views.getCoursesByCategory),
+    path('kategori/<str:category_id>', views.getCoursesByCategory),
 ]
--- courseapp/courses/views.py
+++ courseapp/courses/views.py
@@ -1,8 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
-# Create your views here.
+data = {
+    "urunler": "urunler kategorisine ait urunler",
+    "hakkinda": "hakkinda kategorisine ait urunler"
+}
+
 def home(request):
     return HttpResponse('anasayfa')
-def urunler (request):
-    return HttpResponse('Ürünler')
+
+def kurslar(request):
+    return HttpResponse('<h1>Kurslar</h1>')
+
+def urunler(request):
+    return HttpResponse('urunler')
+
+def hakkında(request, kurs_adi):
+    return HttpResponse(f"{kurs_adi} detay sayfası")
+
+def getCoursesByCategory(request, category):
+    try:
+        category_text = data.get(category, "Kategori bulunamadı")
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound("yanlış kategori seçimi")
+
+def getCoursesByCategoryId(request, category_id):
+    # 1-2-3 =>
+    category_list = list(data.keys())
+    if(category_id > len(category_listt)):
+        return HttpResponseNotFound("yanlış kategori seçimi")
+    redirect_text = category_list[category_id]
+    return redirect('/kurs/kategori/urunler')
